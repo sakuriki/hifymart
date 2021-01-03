81428585f27bf439d591928a34b268bebb91f764
sakuriki <saku.vhh.no1@gmail.com>
2021-01-03 11:39:06 +0700

    Đổi sang dùng vuex-persistedstate

---
+++ client/plugins/vuex-persistedstate.js
@@ -0,0 +1,24 @@
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from "js-cookie";
+import cookie from "cookie";
+
+export default ({ store, req }) => {
+  createPersistedState({
+    paths: ["cart.cart"],
+    storage: {
+      getItem: key => {
+        // See https://nuxtjs.org/guide/plugins/#using-process-flags
+        if (process.server) {
+          const parsedCookies = cookie.parse(req.headers.cookie);
+          return parsedCookies[key];
+        } else {
+          return Cookies.get(key);
+        }
+      },
+      // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+      setItem: (key, value) =>
+        Cookies.set(key, value, { expires: 365, secure: false }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })(store);
+};
