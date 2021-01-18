2cf75826523cca963af7302c8529cd25e3fbced9
sakuriki <saku.vhh.no1@gmail.com>
2021-01-18 16:12:07 +0700

    Update seeder

--- database/seeds/Dev/OrderSeeder.php
+++ database/seeds/Dev/OrderSeeder.php
@@ -5,7 +5,6 @@ use App\Models\User;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\District;
-use App\Models\Province;
 use Illuminate\Support\Arr;
 use App\Models\OrderProduct;
 use Illuminate\Database\Seeder;
@@ -22,15 +21,15 @@ class OrderSeeder extends Seeder
     $faker = Factory::create();
     $products = Product::all();
     $productIds = $products->pluck('id')->toArray();
-    $districts = District::all()->pluck('id')->toArray();
-    $provinces = Province::all()->pluck('id')->toArray();
+    $districts = District::all();
+    $districtIds = $districts->pluck('id')->toArray();
     $users = User::all();
     // $faker->dateTimeBetween('first day of september last year')
-    for ($i = 0; $i < 1000; $i++) {
+    for ($i = 0; $i < 10000; $i++) {
       $user = $faker->boolean(10) ? $users->random() : null;
       $subTotal = 0;
       $orderProduct = [];
-      $time = $faker->dateTimeBetween('-1 year -3 months');
+      $time = $faker->dateTimeBetween('-9 years', '+1 year');
       foreach (Arr::wrap(array_rand($productIds, rand(1, 3))) as $product) {
         $quantity = rand(1, 3);
         $subTotal += $products[$product]->getSalePrice($time->format('Y-m-d H:i:s')) * $quantity;
@@ -43,10 +42,11 @@ class OrderSeeder extends Seeder
       $tax = $subTotal * 0.1;
       $totalAfterDiscount = $subTotal - $discount;
       $total = $totalAfterDiscount > 0 ? $totalAfterDiscount + $tax : 0;
+      $randDistrict = array_rand($districtIds, 1);
       $order = Order::create([
         'user_id' => $user ? $user->id : null,
-        'district_id' => $districts[array_rand($districts, 1)],
-        'province_id' => $provinces[array_rand($provinces, 1)],
+        'district_id' => $districtIds[$randDistrict],
+        'province_id' => $districts[$randDistrict]->province_id,
         'billing_email' => $user ? $user->email : $faker->safeEmail,
         'billing_name' => $user ? $user->name : $faker->name,
         'billing_address' => $faker->unique()->sentence(2),
@@ -59,12 +59,16 @@ class OrderSeeder extends Seeder
         'created_at' => $time,
         'updated_at' => $time
       ]);
-      $orderProduct = collect($orderProduct)->map(function ($item) use ($order) {
+      $orderProduct = array_map(function ($item) use ($order) {
         $item["order_id"] = $order->id;
         return $item;
-      });
+      }, $orderProduct);
+      // $orderProduct = collect($orderProduct)->map(function ($item) use ($order) {
+      //   $item["order_id"] = $order->id;
+      //   return $item;
+      // });
       // return $orderProduct;
-      OrderProduct::insert($orderProduct->toArray());
+      OrderProduct::insert($orderProduct);
     };
     // User::all()->each(function ($user) use ($productIds, $products, $districts, $provinces, $faker) {
     //   $subTotal = 0;
