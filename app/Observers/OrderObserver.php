2128ff96982c22958afd24ace09afaef56fc39ae
sakuriki <saku.vhh.no1@gmail.com>
2021-02-25 23:03:05 +0700

    Update order

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Order;
 use App\Mail\SendInvoice;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class OrderObserver
@@ -17,5 +18,70 @@ class OrderObserver
   public function created(Order $order): void
   {
     Mail::to($order->billing_email)->queue(new SendInvoice($order));
+  }
+
+  /**
+   * Listen to the Order updating event.
+   *
+   * @param  \App\Order  $order
+   * @return void
+   */
+  public function updating(Order $order)
+  {
+    if ($order->isDirty('status')) {
+      if ($order->status == 3) {
+        try {
+          DB::beginTransaction();
+          $order_products = OrderProduct::select('product_id', 'quantity')
+            ->where('order_id', $order->id)
+            ->get()
+            ->keyBy('product_id');
+          $products = Product::whereIn("id", $order_products->pluck('product_id'))->get();
+          foreach ($products as $product) {
+            $product->quantity = $product->quantity - $order_products[$product->id]['quantity'];
+            $product->save();
+          }
+          DB::commit();
+        } catch (\Exception $exception) {
+          DB::rollBack();
+          throw ValidationException::withMessages([
+            'message' => 'Có lỗi khi cập nhật số lượng sản phẩm còn trong kho, vui lòng thử lại'
+          ]);
+        }
+      }
+    }
+  }
+
+  /**
+   * Listen to the Order deleting event.
+   *
+   * @param  \App\Order  $order
+   * @return void
+   */
+  public function deleting(Order $order)
+  {
+    try {
+      DB::beginTransaction();
+      $order_products = OrderProduct::select('product_id', 'quantity')
+        ->where('order_id', $order->id)
+        ->get()
+        ->keyBy('product_id');
+      $products = Product::whereIn("id", $order_products->pluck('product_id'))->get();
+      foreach ($products as $product) {
+        $product->quantity = $product->quantity + $order_products[$product->id]['quantity'];
+        $product->save();
+      }
+      DB::commit();
+    } catch (\Exception $exception) {
+      DB::rollBack();
+      throw ValidationException::withMessages([
+        'message' => 'Có lỗi khi cập nhật số lượng sản phẩm còn trong kho, vui lòng thử lại'
+      ]);
+    }
+  }
+
+  private function updateQuantity($order)
+  {
+    # code...
   }
 }
